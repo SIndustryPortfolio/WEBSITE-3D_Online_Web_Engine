bcdf6ec2bdd5e131b62b8abe6c8c7ea88f6ec53c
ShayansRepositories <shayan.ahmed85@outlook.com>
2026-02-10 13:30:33 +0000

    MFA Form fix

--- forms/mfaForm.py
+++ forms/mfaForm.py
@@ -9,5 +9,5 @@ from wtforms.validators import DataRequired
 class MFAForm(FlaskForm):
     # CORE
     otp = StringField("otp", render_kw = {"class": "Input", "id": "otpInput1"}, validators = [DataRequired()])
-    submit = SubmitField("submit", render_kw = {"class" : "Input", "value": "Verify", "id" : "otpFormSubmit1"})
+    submit = SubmitField("submit", render_kw = {"class" : "btn btn-success", "value": "Verify", "id" : "otpFormSubmit1"})
     #cancel = SubmitField("cancel", render_kw = {"class": "btn btn-secondary", "value": "Cancel", "id" : "formCancel1"})
